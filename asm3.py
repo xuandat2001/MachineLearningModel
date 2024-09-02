5d85adab6be8bce9b3111d6bfcd15b0940d8bfa1
callystale <119844030+callystale@users.noreply.github.com>
2024-09-02 20:28:33 +0700

    Add files via upload

--- asm3.py
+++ asm3.py
@@ -20,8 +20,8 @@ import joblib
 
 
 # In[2]: STEP 2. GET THE DATA (DONE). LOAD DATA
-raw_data = pd.read_csv(r'C:\RMIT Programing\Python Programming\Asm3\datasets\CarDetailsV3.csv')
-
+# Note: the path has to be edited corresponding to where it is on the local computer
+raw_data = pd.read_csv(r'/Users/callysta/Documents/FoundOfAI/GroupAssignment/datasets/CarDetailsV3.csv')
 
 # In[3]: STEP 3. DISCOVER THE DATA TO GAIN INSIGHTS
 #region
@@ -34,3 +34,25 @@ print('\n____________ Counts on a feature ____________')
 print(raw_data['fuel'].value_counts()) 
 print('\n____________ Statistics of numeric features ____________')
 print(raw_data.describe())    
+# %%
+# 3.2 Scatter plot b/w 2 features
+# if 1:
+#     raw_data.plot(kind="scatter", y="selling_price", x="year", alpha=0.2)
+#     plt.savefig('figures/scatter_1_feat.png', format='png', dpi=300)
+#     plt.show()    
+if 1:
+    raw_data.plot(kind="scatter", y="selling_price", x="km_driven", alpha=0.2)
+    plt.savefig('figures/scatter_selling_price_km_driven.png', format='png', dpi=300)
+    plt.show()  
+
+
+# %%
+# # 3.3 Scatter plot b/w every pair of features
+if 1:
+    from pandas.plotting import scatter_matrix 
+    features_to_plot = ["selling_price", "year", "km_driven", "max_power"]
+    scatter_matrix(raw_data[features_to_plot], figsize=(12, 8)) # Note: histograms on the main diagonal
+    plt.savefig('figures/scatter_matrix_car_features.png', format='png', dpi=300)
+    plt.show()
+
+# %%
